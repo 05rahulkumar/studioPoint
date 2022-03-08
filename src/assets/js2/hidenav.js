7057b2a7a75d4c97bc23e305023d1e5301d7b47f
rahul0016 <rahul@srchoutsoftware.com>
2022-03-08 19:09:27 +0530

    navbar bg color with logo web+mob view

---
+++ src/assets/js2/hidenav.js
@@ -0,0 +1,8 @@
+$(window).scroll(function() {
+  if ($(this).scrollTop()>500){
+      $('.myimgDiv').hide(00);
+   }
+  else{
+    $('.myimgDiv').show(00);
+   }
+});
--- src/assets/js2/nav.js
+++ src/assets/js2/nav.js
@@ -1,9 +1,4 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-}
+window.onscroll = () => {
+  const span = document.querySelector('#navbar');
+  if(this.scrollY <= 500) span.className = ''; else span.className = 'scroll';
+};
